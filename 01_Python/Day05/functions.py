e55540e1f3333365bf8a65f91a3b5d6170056d16
RISHITH REDDY <rishithreddy583@gmail.com>
2026-07-21 23:09:37 +0530

    Day 5 Revision : completed python functions practice

--- 01_Python/Day05/functions.py
+++ 01_Python/Day05/functions.py
@@ -1,6 +1,29 @@
+"""
 def add():
-               x = int(input("Enter the x value: "))
-               y = int(input("Enter the y value: "))
-               result = x + y
-               print("Addition = ",result)
-add()
+           result = a + b
+           print(f" a + b = {result}")
+a = int(input("Enter the number: "))
+b = int(input("dEnter the number: "))
+
+add() 
+""" 
+
+""" 
+def even_odd(n):
+               if n%2 == 0 :
+                              return "even"
+               return "odd"
+n = int(input("Enter the number: "))
+answer = even_odd(n)
+print ("the number is ", answer)
+"""
+""" 
+def fact(n):
+               fact_num = 1
+               for i in range(1,n+1):
+                              fact_num *= i
+               return fact_num
+n = int(input("Enter the number: "))
+answer = fact(n)
+print("Factorial = ", answer)                           
+"""                            
